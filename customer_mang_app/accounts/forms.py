ebf9ee3b36768d420d3ed18f7315be4468b43d8f
devika30 <olkardevika69@gmail.com>
2020-08-02 23:08:35 +0530

    updated the chnages

--- customer_mang_app/accounts/forms.py
+++ customer_mang_app/accounts/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
-from .models import *
+from .models import Customer,Order,User
 
 class CustomerForm(ModelForm):
 	class Meta:
--- customer_mang_app/cma/settings.py
+++ customer_mang_app/cma/settings.py
@@ -77,14 +77,11 @@ WSGI_APPLICATION = 'cma.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Django_demo',
-        'HOST':'127.0.0.1',
-        'PORT':'3306',
-        'USER':'root',
-        'PASSWORD':''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
