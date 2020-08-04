0124ea259d992b7e891d5563e3b1eca6c2844e56
devika30 <olkardevika69@gmail.com>
2020-08-04 12:57:26 +0530

    updated files

--- customer_mang_app/accounts/models.py
+++ customer_mang_app/accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User 
+from django.db.models.signals import post_save
+
 # Create your models here.
 
 class Customer(models.Model):
@@ -8,7 +10,7 @@ class Customer(models.Model):
 	name = models.CharField(max_length=200, null=True)
 	phone = models.CharField(max_length=200, null=True)
 	email = models.CharField(max_length=200, null=True)
-	profile_pic=models.ImageField(null=True,blank=True)
+	profile_pic=models.ImageField(null=True,blank=True,default="pic.png")
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 
 
@@ -39,13 +41,14 @@ class Product(models.Model):
 		return self.name
 
 
-STATUS = (
+
+
+class Order(models.Model):
+	STATUS = (
 			('Pending', 'Pending'),
 			('Out for delivery', 'Out for delivery'),
 			('Delivered', 'Delivered'),
 			)
-
-class Order(models.Model):
 	customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
 	product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
@@ -53,3 +56,29 @@ class Order(models.Model):
     
 	def __str__(self):
 		return str(self.product.name)
+
+# class Profile(models.Model):
+# 	user=models.OneToOneField(User,on_delete=models.CASCADE,blank=True,null=True)
+# 	first_name=models.CharField(max_length=100, null=True, blank=True)
+# 	last_name=models.CharField(max_length=100, null=True, blank=True)
+# 	phone=models.CharField(max_length=100,null=True,blank=True)
+
+# 	def __str__(self):
+# 		return str(self.first_name)
+	
+# def create_profile(sender,instance,created,**kwargs):
+# 	if created:
+# 		Profile.objects.create(user=instance)
+# 		print("Profile created")
+
+# post_save.connect(create_profile,sender=User)
+
+# def update_profile(sender,instance,created,**kwargs):
+# 	if created==False:
+# 		instance.profile.save()
+# 		print("Profile updated")
+# post_save.connect(update_profile,sender=User)
+
+
+
+	
